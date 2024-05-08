44134b40620242d9c858dafd2700e65b9f55f7fc
PaulaVelezz <velezpaula.a@gmail.com>
2024-05-08 02:15:05 -0300

    ReactiveForms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Ispc-Login';
+  form!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      usuario: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  onSubmit(event: Event) {
+    {
+      console.log(this.form.value)
+    }
+  }
 }
